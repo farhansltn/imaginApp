1f04eca05b7077cac1504d337dffd4b1ab22c471
farhansltn <farhanfarsul@gmail.com>
2024-03-14 08:55:10 +0700

    fixing commit3.2

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignIn, SignUp } from '@clerk/nextjs'
 import React from 'react'
 
 const SignInPage = () => {
   return (
-    <SignUp/>
+    <SignIn/>
   )
 }
 
